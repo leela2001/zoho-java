c333a06769fecf29af0868b395da68d2a38e0a49
LEELA D <88545963+leela2001@users.noreply.github.com>
2024-08-13 15:28:44 +0530

    Update pattern2.java

--- patternh/pattern2.java
+++ patternh/pattern2.java
@@ -15,3 +15,21 @@ class HelloWorld {
         }
     }
 }
+// ######## 2nd method ##################
+// import java.util.*;
+// class HelloWorld {
+//     public static void main(String[] args) {
+//         System.out.println("Try programiz.pro");
+//         Scanner sc =  new Scanner(System.in);
+//         System.out.println("Enter num : ");
+//         int n = sc.nextInt();
+//         for(int row = 1; row<=n; row++)
+//         {
+//             for(int col= i; col<=n; col++)
+//             {
+//                 System.out.print(" * ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
