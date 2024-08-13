586ed40e08c358e8c17de8de3472e35bc7479722
LEELA D <88545963+leela2001@users.noreply.github.com>
2024-08-13 17:32:46 +0530

    Create pattern6.java

---
+++ patternh/pattern6.java
@@ -0,0 +1,30 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        Scanner sc =  new Scanner(System.in);
+        System.out.print("Enter num : ");
+        int n = sc.nextInt();
+        System.out.println("");
+        for(int row = 1; row<=n; row++)
+        {
+            for(int sp =1; sp <= row-1; sp++)
+            {
+                System.out.print("_ ");//  space for organize
+                
+            }
+            for(int col= 1; col<=n-row+1; col++)
+            {
+                System.out.print("* ");
+            }
+            for(int col= 1; col<=n-row; col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+            
+        }
+    }
+}
