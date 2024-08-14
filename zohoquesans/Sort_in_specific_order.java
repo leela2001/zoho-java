dbc3a06f88cd34872ccbe80c365318bb9c27f457
LEELA D <88545963+leela2001@users.noreply.github.com>
2024-08-14 16:04:56 +0530

    Create Sort_in_specific_order.java

---
+++ zohoquesans/Sort_in_specific_order.java
@@ -0,0 +1,35 @@
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+      
+	        int[] arr = {0,1, 2, 3, 5, 4, 7, 10};
+	        sortit(arr);
+	        System.out.print("Odd(Desc) and Even(Asc) : ");
+	        for(int i :arr)
+	        {
+	            System.out.print(i+" ");
+	        }
+    }
+    public static int[] sortit(int[] arr)
+    {
+        oddnegative(arr);
+        Arrays.sort(arr);
+        oddnegative(arr);
+        return arr;
+    }
+    public static void oddnegative(int[] arr)
+    {
+        for(int i = 0; i<arr.length; i++)
+        {
+            if(arr[i]%2!=0)
+            {
+                arr[i]*=-1;
+            }
+        }
+    }
+    
+    
+}
