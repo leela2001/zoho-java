33f66f82ec10729b3a18242d5b3e0203f3ffda78
LEELA D <88545963+leela2001@users.noreply.github.com>
2024-08-15 21:36:06 +0530

    Update Alternative_Sorting.java

--- zohoquesans/Alternative_Sorting.java
+++ zohoquesans/Alternative_Sorting.java
@@ -1,1 +1,30 @@
-nknk
+
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+
+class Solution {
+    public static ArrayList<Integer> alternateSort(int[] arr) {
+
+        // Your code goes here
+        
+        int sp=0;
+        int ep=arr.length-1;
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        Arrays.sort(arr);
+        while(sp<=ep)
+        {
+            res.add(arr[ep--]);
+            if(sp<ep)
+            {
+                res.add(arr[sp++]);
+            }
+        }
+        
+        return res;
+    }
+}
