2e3a48e18ef7d06417055c7449d9bd123c55c6ba
LEELA D <88545963+leela2001@users.noreply.github.com>
2024-08-15 21:09:43 +0530

    Update Total_count.java

--- zohoquesans/Total_count.java
+++ zohoquesans/Total_count.java
@@ -1,2 +1,24 @@
-bjggj
-  
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+
+
+class Solution {
+    int totalCount(int k, int[] arr) {
+        // code here
+        int res = 0;
+        for(int  val: arr)
+        {
+            res += val/k;
+            if(val%k != 0)
+            {
+                res++;
+            }
+        }
+        return res;
+    }
+}
+
