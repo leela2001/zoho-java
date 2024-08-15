2750f1830a6d37770c15928a653573981cb6b649
LEELA D <88545963+leela2001@users.noreply.github.com>
2024-08-15 15:10:37 +0530

    Update Count_the_characters.java

--- zohoquesans/Count_the_characters.java
+++ zohoquesans/Count_the_characters.java
@@ -1,1 +1,34 @@
-hjh
+
+import java.io.*;
+import java.util.*;
+
+
+class Sol
+{
+    int getCount (String S, int N)
+    {
+        // your code here
+        
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put(S.charAt(0), 1);
+        for(int i = 1; i< S.length(); i++)
+        {
+            if(S.charAt(i) != S.charAt(i-1))
+            {
+                Integer cur_val = map.getOrDefault(S.charAt(i),0);
+                map.put(S.charAt(i),cur_val+1);
+            }
+        }
+        
+        int count =0;
+        for(Integer val : map.values())
+        {
+            if(val==N)
+            {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
